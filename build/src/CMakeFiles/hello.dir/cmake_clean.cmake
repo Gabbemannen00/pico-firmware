7771f50ee6314be72a0e81f5863743d11704acbb
Gabbemannen00 <gabriel-carlsson@hotmail.se>
2025-04-15 13:13:37 +0200

    Updated CMake-files by adding theircontent

---
+++ build/src/CMakeFiles/hello.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "/Users/gabrielcarlsson/ws/pico-firmware/bin/hello"
+  "/Users/gabrielcarlsson/ws/pico-firmware/bin/hello.pdb"
+  "CMakeFiles/hello.dir/main.cpp.o"
+  "CMakeFiles/hello.dir/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hello.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
